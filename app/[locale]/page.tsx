5a0bd0602a28f51fb916d52c461822822896eba5
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2023-12-08 22:59:12 +0100

    fix

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,11 @@
-import { Hero } from '@/components/sections'
 import {useTranslations} from 'next-intl';
 
 export default function Home() {
   const t = useTranslations('Navbar');
 
   return (
-    <main>
-      <Hero />
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className='text-2xl'>{t('button')}</h1>
     </main>
   )
 }
