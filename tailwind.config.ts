b638a31c0ca5a28a8a37232308837d446a806d94
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2023-12-08 16:18:02 +0100

    fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#F91942',
+        gray: '#191B20'
+      },
     },
   },
   plugins: [],
