c9b6fee64c46bde468ab0a0455fe500ad1de7d70
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2023-12-11 11:38:02 +0100

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -12,7 +12,7 @@ export const Input = ({ className, register, ...props }: InputProps) => {
 	return (
 		<div className="w-full relative">
 			<input
-				className={cn('bg-transparent text-lg md:text-xl font-light w-full border-b border-white pb-[5px] outline-none', className)}
+				className={cn('bg-transparent text-lg md:text-xl font-light w-full border-b border-white pb-[5px] outline-none rounded-none', className)}
 				{...register}
 				{...props}
 			/>
